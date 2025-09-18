28c95c605fb466950300c9ad02b94128b0667a88
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-09-18 22:37:48 +0800

    Fix: Fixed flared game logic in GameVerdict function

--- src/utils/GameMaster.ts
+++ src/utils/GameMaster.ts
@@ -2,7 +2,8 @@ export default function GameVerdict(
   board: string[],
   currentNumOfMoves: number
 ) {
-  const symbol = currentNumOfMoves % 2 === 0 ? "X" : "O";
+  const symbol = currentNumOfMoves % 2 ? "X" : "O";
+  let hasWinner = false;
   const verdict = {
     gameDone: false,
     winner: "",
@@ -10,54 +11,67 @@ export default function GameVerdict(
 
   if (board[0] === symbol && board[1] === symbol && board[2] === symbol) {
     verdict.gameDone = true;
-    verdict.winner = currentNumOfMoves % 2 === 0 ? "Player 1" : "Player 2";
+    hasWinner = true;
   } else if (
     board[3] === symbol &&
     board[4] === symbol &&
     board[5] === symbol
   ) {
     verdict.gameDone = true;
+    hasWinner = true;
   } else if (
     board[6] === symbol &&
     board[7] === symbol &&
     board[8] === symbol
   ) {
     verdict.gameDone = true;
+    hasWinner = true;
   } else if (
     board[0] === symbol &&
     board[3] === symbol &&
     board[6] === symbol
   ) {
     verdict.gameDone = true;
+    hasWinner = true;
   } else if (
     board[1] === symbol &&
     board[4] === symbol &&
     board[7] === symbol
   ) {
     verdict.gameDone = true;
+    hasWinner = true;
   } else if (
     board[2] === symbol &&
     board[5] === symbol &&
     board[8] === symbol
   ) {
     verdict.gameDone = true;
+    hasWinner = true;
   } else if (
     board[0] === symbol &&
     board[4] === symbol &&
     board[8] === symbol
   ) {
     verdict.gameDone = true;
+    hasWinner = true;
   } else if (
     board[2] === symbol &&
     board[4] === symbol &&
     board[6] === symbol
   ) {
     verdict.gameDone = true;
+    hasWinner = true;
   } else {
     verdict.gameDone = currentNumOfMoves === 9 ? true : false;
+    hasWinner = false;
   }
-  
-  verdict.winner =
-    currentNumOfMoves < 9 ? (symbol === "X" ? "Player 1" : "Player 2") : "";
+
+  if (verdict.gameDone && hasWinner) {
+    verdict.winner = currentNumOfMoves % 2 ? "Player 1" : "Player 2";
+  }
+
+  if (verdict.gameDone && !hasWinner) {
+    verdict.winner = "";
+  }
   return verdict;
 }
