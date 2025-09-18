6f81a1747bc8f18754a2c64db124102720dae056
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-09-18 13:23:25 +0800

    Created layout for the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <h1>Tic Tac Toe</h1>
+        <p>Play Now!</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="main">
+        <div className="game">
+          <div className="board">Board here</div>
+        </div>
+        <div className="game-info">
+          <div className="move-history">
+            <h3>Moves</h3>
+            <p>placeholder move</p>
+          </div>
+          <div className="verdict">
+            <p>Player 1 Wins!</p>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button className="reset-button">Play Again</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
